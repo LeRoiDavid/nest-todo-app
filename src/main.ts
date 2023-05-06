af5544d9114fdadf3bd9fdc21471c7e2445ca8c8
Daouda DIONE <Daoudadione79@gmail.com>
2023-05-06 09:01:20 +0000

    Test webhooks

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log("Hello there");
+  
   await app.listen(3200);
 }
 bootstrap();
