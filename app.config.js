3a4c40ae1f6435a7b280b896195a2d2b7f3b890b
MacBook Air <macbookair@air-de-macbook.home>
2023-05-06 07:21:10 +0000

    Add app.config.js file for pm2

---
+++ app.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'todo-app',
+      script: 'main.js',
+      instances: '1',
+      exec_mode: 'cluster',
+      max_memory_restart: '2G',
+      watch: false,
+      ignore_watch: ['node_modules'],
+      node_args: '--max_old_space_size=8192 --expose-gc',
+    },
+  ],
+};
